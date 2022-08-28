0b643c0b4bf8664c671c740550f643846a8477a8
Moirangthem Romenkumar Singh <83605713+romenrake@users.noreply.github.com>
2022-08-28 16:32:14 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String[] args){
+        int x=20;
+        int y=30;
+        int z= x+y;
+        System.out.println(z);
+        System.out.println("Hello world!");
+    }
+
+}
