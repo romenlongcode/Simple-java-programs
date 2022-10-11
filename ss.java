bfdc1959b6538f65718db33abe259780206f6269
Moirangthem Romenkumar Singh <83605713+romenrake@users.noreply.github.com>
2022-10-11 23:00:55 +0530

    Add files via upload

---
+++ ss.java
@@ -0,0 +1,10 @@
+public class ss {
+    public static void main(String[] args) {
+       String ans= greet();
+        System.out.print(ans);
+    }
+    static  String greet(){
+        String greeting="How are you doing?";
+        return  greeting;
+    }
+}
