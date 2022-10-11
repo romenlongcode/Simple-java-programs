f2f5809b037ac0524392655db1784be61a3aab12
Moirangthem Romenkumar Singh <83605713+romenrake@users.noreply.github.com>
2022-10-11 22:59:33 +0530

    Add files via upload

---
+++ svarrg.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class svarrg {
+    public static void main(String[] args) {
+        fun(3,5,9,0,6);
+
+    }
+    static  void fun(int...v) {
+        System.out.println(Arrays.toString(v));
+    }}
